87c0ee198d5b2aae9ae4962669348299f0ffce1a
tanihito <tanihito@livedoor.com>
2012-09-10 22:46:21 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   has_many :followed_users, through: :relationships,         source: :followed
   has_many :followers,      through: :reverse_relationships, source: :follower
 
-  before_save { |user| user.email = email.downcase }  
+  before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
